28e04bc6f012467bdb3c58767f7db8606f15b2b4
Duck <61479436+duck-dev@users.noreply.github.com>
2022-02-05 20:58:32 +0100

    Clean up ViewLocator.cs
    - Cleaned ViewLocator.cs up (lambda-expression instead of brackets when possible and reasonable, guard-clause instead of if-else etc.).

--- src/ViewLocator.cs
+++ src/ViewLocator.cs
@@ -13,18 +13,11 @@ namespace VocabularyTrainer
             var type = Type.GetType(name);
 
             if (type != null)
-            {
                 return (Control) Activator.CreateInstance(type)!;
-            }
-            else
-            {
-                return new TextBlock {Text = "Not Found: " + name};
-            }
+            
+            return new TextBlock {Text = "Not Found: " + name};
         }
 
-        public bool Match(object data)
-        {
-            return data is ViewModelBase;
-        }
+        public bool Match(object data) => data is ViewModelBase;
     }
 }
