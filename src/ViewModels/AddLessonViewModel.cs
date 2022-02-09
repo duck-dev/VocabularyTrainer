633c1bd63b47e9ad2e4dc791dc0f79ccfcb408ad
Duck <61479436+duck-dev@users.noreply.github.com>
2022-02-09 21:33:47 +0100

    Style button for adding new word (when creating lesson)
    - This button has the `selectable-button-effect` class/selector for the effects "on pointer over".
    - Styled the button completely (not done yet, of course subject to change).
    - Added two `string` properties for the name and description of a new lesson to the View Model.

--- src/ViewModels/AddLessonViewModel.cs
+++ src/ViewModels/AddLessonViewModel.cs
@@ -2,6 +2,7 @@ namespace VocabularyTrainer.ViewModels
 {
     public class AddLessonViewModel : ViewModelBase
     {
-        
+        private string? CurrentName { get; set; }
+        private string? CurrentDescription { get; set; }
     }
 }
