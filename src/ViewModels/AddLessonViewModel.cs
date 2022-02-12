18dbe9e8603bfcd0bfdc7e3f764960a662216413
Duck <61479436+duck-dev@users.noreply.github.com>
2022-02-12 22:32:04 +0100

    Proper design basics for AddLessonView.axaml
    - Earlier, the UI elements in the AddLessonView.axaml were aligned inside a `StackPanel`, which allowed dynamic listing, but controlling the dynamic size of certain elements is impossible. That's why the parent `StackPanel` has been replaced by a `Grid`.
    - Everything is wrapped inside a `ScrollViewer`, so the user can scroll down if the window isn't high enough to display all elements at once.
    - The two `TextBox` elements (for the name and description) don't share the same margin of 0,20 anymore, but both have their separate margins to avoid duplicate margins and disproportionate spaces.
    - Changed the selector name "word-panel" to "word-item-panel" to better distinguish it from other selectors with a similar name.
    - Added a `Words` property (of type `ObservableCollection<T>`), which will (temporarily) store all words. It's generic type is currently `object`, because there is no specific type that represents a "word" yet.
    
    - Added a margin to the button for adding a new word.
    - Reduced it's height from 100 to 75.
    - Commented the experimental `BorderBrush` definition out. It still doesn't work.
    - Defined some properties (HorizontalAlignment, Height and Margin) of the "word-item-panel" selector (the word-item).

--- src/ViewModels/AddLessonViewModel.cs
+++ src/ViewModels/AddLessonViewModel.cs
@@ -1,8 +1,11 @@
+using System.Collections.ObjectModel;
+
 namespace VocabularyTrainer.ViewModels
 {
     public class AddLessonViewModel : ViewModelBase
     {
         private string? CurrentName { get; set; }
         private string? CurrentDescription { get; set; }
+        private ObservableCollection<object>? Words { get; } = new(); // TODO: Change to word type
     }
 }
