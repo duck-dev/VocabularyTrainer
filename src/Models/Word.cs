7d81cf5e64095ddb16efd31cc9519485c91eef01
Duck <61479436+duck-dev@users.noreply.github.com>
2022-03-19 22:44:45 +0100

    Add functionality for synonyms/antonyms
    - Added the functionality for adding "thesaurus items" (synonyms, antonyms) to the `Word` class.
    - Added a method that removes the current word from it's associated lesson.

--- src/Models/Word.cs
+++ src/Models/Word.cs
@@ -1,12 +1,44 @@
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using VocabularyTrainer.Models.ItemStyleControls;
+using VocabularyTrainer.ViewModels;
 
 namespace VocabularyTrainer.Models
 {
     public class Word
     {
+        public Word()
+        {
+            ThesaurusTitleDefinitions = new[]
+            {
+                new Tuple<string, string, ItemStyleBase<Word>>("Synonyms:", "Add Synonym", new SynonymStyle(this)),
+                new Tuple<string, string, ItemStyleBase<Word>>("Antonyms:", "Add Antonym", new AntonymStyle(this))
+            };
+        }
+        
+        private Tuple<string, string, ItemStyleBase<Word>>[] ThesaurusTitleDefinitions { get; }
+        
         internal string? Term { get; set; }
         internal string? Definition { get; set; }
-        internal List<Word>? Synonyms { get; }
-        internal List<Word>? Antonyms { get; }
+        internal ObservableCollection<KeyValuePair<string,string>> Synonyms { get; } = new();
+        internal ObservableCollection<KeyValuePair<string,string>> Antonyms { get; } = new();
+        
+        internal AddLessonViewModel? ParentLesson { get; init; }
+
+        private void RemoveWord() => ParentLesson?.Words.Remove(this);
+
+        public void AddThesaurusItem(ItemStyleBase<Word> type)
+        {
+            switch (type)
+            {
+                case SynonymStyle:
+                    Synonyms.Add(new KeyValuePair<string, string>());
+                    break;
+                case AntonymStyle:
+                    Antonyms.Add(new KeyValuePair<string, string>());
+                    break;
+            }
+        }
     }
 }
--- src/ViewModels/AddLessonViewModel.cs
+++ src/ViewModels/AddLessonViewModel.cs
@@ -7,11 +7,14 @@ namespace VocabularyTrainer.ViewModels
     {
         private string? CurrentName { get; set; }
         private string? CurrentDescription { get; set; }
-        private ObservableCollection<Word> Words { get; } = new() { new Word() };
+        internal ObservableCollection<Word> Words { get; } = new() { new Word() };
 
         private void AddWord()
         {
-            var newWord = new Word();
+            var newWord = new Word
+            {
+                ParentLesson = this
+            };
             Words.Add(newWord);
         }
     }
