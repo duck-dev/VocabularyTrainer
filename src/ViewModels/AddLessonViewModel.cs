8e86650cecad4378630b81b03f2848984d1cb919
Duck <61479436+duck-dev@users.noreply.github.com>
2022-03-23 22:08:19 +0100

    Add word counter in heading (create lesson)
    - Complemented the heading (Words) in the add-page for creating a lesson with a counter that counts the words present in the lesson. E.g. "Words (5 items):"
        - The term "items" is adjusted, based on whether the count of items is 1 or more than 1 (item/items).
    - Added a top-margin, besides the already existing bottom-margin to the entire parent of the synonyms and antonyms of a vocabulary item (word).

--- src/ViewModels/AddLessonViewModel.cs
+++ src/ViewModels/AddLessonViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using ReactiveUI;
 using VocabularyTrainer.Interfaces;
 using VocabularyTrainer.Models;
 
@@ -10,6 +11,7 @@ namespace VocabularyTrainer.ViewModels
         {
             VocabularyItems.CollectionChanged += (sender, args) =>
             {
+                this.RaisePropertyChanged(nameof(AdjustableItemsString));
                 for (int i = 0; i < VocabularyItems.Count; i++)
                     VocabularyItems[i].Index = i;
             };
@@ -18,6 +20,7 @@ namespace VocabularyTrainer.ViewModels
         private string? CurrentName { get; set; }
         private string? CurrentDescription { get; set; }
         public ObservableCollection<Word> VocabularyItems { get; } = new() { new Word() };
+        private string AdjustableItemsString => VocabularyItems.Count == 1 ? "item" : "items";
 
         private void AddWord()
         {
