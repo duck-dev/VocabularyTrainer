8d6889aeaa67add7167409e1af100df185fe75d5
Duck <61479436+duck-dev@users.noreply.github.com>
2022-03-27 22:47:19 +0200

    Create `DataManager` class with list of lessons
    - Created a static `DataManager` class including a list of lessons. This will be extended in the near future.

---
+++ src/Models/DataManager.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace VocabularyTrainer.Models
+{
+    public static class DataManager
+    {
+        internal static List<Lesson> Lessons { get; } = new();
+    }
+}
