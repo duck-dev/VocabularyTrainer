cbf408d0378517d665add28240c668f7b30a3e59
Duck <61479436+duck-dev@users.noreply.github.com>
2022-03-30 14:16:21 +0200

    Implement logic for saving and loading data
    - Implemented the logic for saving and loading data.
        - Created two methods, one for loading, one for saving data (lessons) in DataManager.cs.
        - Created a `_filePath` field, which contains the path to the file with the saved lessons (in DataManager.cs).
        - Created a variable that returns the parent path of all saved files (lessons, settings etc.).

--- src/Models/DataManager.cs
+++ src/Models/DataManager.cs
@@ -1,9 +1,32 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using VocabularyTrainer.UtilityCollection;
 
 namespace VocabularyTrainer.Models
 {
     public static class DataManager
     {
-        internal static List<Lesson> Lessons { get; } = new();
+        private static readonly string _filePath = Path.Combine(Utilities.FilesParentPath, "Lessons.json");
+        
+        internal static List<Lesson> Lessons { get; private set; } = new();
+
+        internal static void LoadData()
+        {
+            if (!File.Exists(_filePath))
+                return;
+
+            string content = File.ReadAllText(_filePath);
+            var deserializedList = JsonSerializer.Deserialize<List<Lesson>>(content);
+            if(deserializedList is not null)
+                Lessons = deserializedList;
+        }
+
+        internal static void SaveData()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(Lessons, options);
+            File.WriteAllText(_filePath, jsonString);
+        }
     }
 }
--- src/UtilityCollection/UniversalUtilities.cs
+++ src/UtilityCollection/UniversalUtilities.cs
@@ -1,7 +1,21 @@
+using System.IO;
+
 namespace VocabularyTrainer.UtilityCollection
 {
     public static partial class Utilities
     {
+        /// <summary>
+        /// The parent path of all settings- and data-files
+        /// </summary>
+        public static string FilesParentPath
+        {
+            get
+            {
+                var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                return directory ?? throw new DirectoryNotFoundException("Directory name of the currently executing assembly is null.");
+            }
+        }
+        
         /// <summary>
         /// Log a message to the console (for debugging purposes).
         /// </summary>
