3ba9b410f1cf837d8b6411647f1d26f86507592a
Duck <61479436+duck-dev@users.noreply.github.com>
2022-03-31 23:41:14 +0200

    Delete SingleVocabularyItem.cs
    - Deleted the SingleVocabularyItem.cs file and with that the `SingleVocabularyItem` class too, because it doesn't differ from using `VocabularyItem`. After the `Definition` property was moved to `VocabularyItem`, this class lost it's function.

--- src/Models/VocabularyItems/SingleVocabularyItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-
-namespace VocabularyTrainer.Models
-{
-    public class SingleVocabularyItem : VocabularyItem
-    {
-        public SingleVocabularyItem(IList? containerCollection = null) : base(containerCollection) { }
-    }
-}
