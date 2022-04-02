dc0633332d07fa19041d327fe16c341ec401e8f1
Duck <61479436+duck-dev@users.noreply.github.com>
2022-04-02 21:22:12 +0200

    Design and construct lesson view
    - Designed and constructed the lesson view with all the information and all words of the opened lesson, as well as a button for editing it. The design is definitely not final, but quite a good draft for now.
    - Changed the `Lesson` class to implement the `IVocabularyContainer<T>` dictionary with T being `Word`.
    - Added a new color resource in BaseResources.axaml: MainRed (#FF0000).

--- src/Models/Lesson.cs
+++ src/Models/Lesson.cs
@@ -1,23 +1,21 @@
 using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
+using VocabularyTrainer.Interfaces;
 
 namespace VocabularyTrainer.Models
 {
-    public class Lesson
+    public class Lesson : IVocabularyContainer<Word>
     {
         [JsonConstructor]
-        public Lesson(string name, string description, ObservableCollection<Word>? words)
+        public Lesson(string name, string description, ObservableCollection<Word> vocabularyItems)
         {
             this.Name = name;
             this.Description = description;
-            
-            if (words is null)
-                return;
-            this.Words = new ObservableCollection<Word>(words);
+            this.VocabularyItems = new ObservableCollection<Word>(vocabularyItems);
         }
         
         public string Name { get; private set; }
         public string Description { get; private set; }
-        public ObservableCollection<Word>? Words { get; } = new();
+        public ObservableCollection<Word> VocabularyItems { get; }
     }
 }
--- src/ViewModels/LessonViewModel.cs
+++ src/ViewModels/LessonViewModel.cs
@@ -1,7 +1,13 @@
+using VocabularyTrainer.Models;
+
 namespace VocabularyTrainer.ViewModels
 {
     public class LessonViewModel : ViewModelBase
     {
+        public LessonViewModel(Lesson lesson)
+            => this.CurrentLesson = lesson;
         
+        private Lesson CurrentLesson { get; }
+        private string AdjustableItemsString => CurrentLesson.VocabularyItems.Count == 1 ? "item" : "items";
     }
 }
