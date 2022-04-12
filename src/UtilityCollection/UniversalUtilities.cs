57f09ec6fb4170afcf179c19327cd3634593f58e
Duck <61479436+duck-dev@users.noreply.github.com>
2022-04-12 15:35:32 +0200

    Add XML-documentation to utility-function
    - Added XML-documentation to `AddChangedItems<T>` in UniversalUtilities.cs.

--- src/UtilityCollection/UniversalUtilities.cs
+++ src/UtilityCollection/UniversalUtilities.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,15 @@ namespace VocabularyTrainer.UtilityCollection
         /// <param name="message">The message to be logged as a string.</param>
         public static void Log(string? message) => System.Diagnostics.Trace.WriteLine(message);
         
+        /// <summary>
+        /// Add added/removed elements of a collection whose changes should be resettable to a list
+        /// that is used to reset the changes if necessary.
+        /// </summary>
+        /// <param name="collection">The collection, which will keep track of all changes to the original collection
+        /// (added/removed items), so it can be used to reset those changes.</param>
+        /// <param name="args">Arguments of the CollectionChanged event of a <see cref="ObservableCollection{T}"/>
+        /// with the changed (added/removed) items and other information about the changes.</param>
+        /// <typeparam name="T">The type of the collection items.</typeparam>
         public static void AddChangedItems<T>(ICollection<T> collection, NotifyCollectionChangedEventArgs args)
         {
             // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
