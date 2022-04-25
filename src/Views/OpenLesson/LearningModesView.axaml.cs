afd6cb60ddad51a0665a2ee94fd5d70b50971ed4
Duck <61479436+duck-dev@users.noreply.github.com>
2022-04-25 22:22:28 +0200

    Create LearningModesView.axaml
    - Created a new UserControl "LearningModesView.axaml" with it's code-behind "LearningModesView.axaml.cs". This view will contain a list of selectable modes for learning the vocabulary of a selected lesson.

---
+++ src/Views/OpenLesson/LearningModesView.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VocabularyTrainer.Views.OpenLesson
+{
+    public class LearningModesView : UserControl
+    {
+        public LearningModesView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
