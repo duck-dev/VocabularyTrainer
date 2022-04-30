e6bdb27dab1f0c51f0e13557ac36b39426f18702
Duck <61479436+duck-dev@users.noreply.github.com>
2022-04-30 22:27:21 +0200

    Simplify toggling learning mode description
    - Simplified toggling the learning mode description visibility a lot.
        - `IsVisible` simply binds to `IsPointerOver` of the `Button`-parent.
        - Removed the `PointerEnter` and `PointerLeave` events.
        - Removed the event handlers.
        - Removed the `DescriptionEnabled` property and it's backing field from the `LearningModeItem` class.
        - `LearningModeItem` doesn't implement `INotifyPropertyChangedHelper` anymore and all it's members have been removed (event, event handler), as they're not needed anymore.

--- src/Models/LearningModeItem.cs
+++ src/Models/LearningModeItem.cs
@@ -1,24 +1,18 @@
 using System;
-using System.ComponentModel;
 using System.IO;
 using System.Reactive;
-using System.Runtime.CompilerServices;
 using Avalonia;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using ReactiveUI;
-using VocabularyTrainer.Interfaces;
 using Path = System.IO.Path;
 
 namespace VocabularyTrainer.Models
 {
-    public class LearningModeItem : INotifyPropertyChangedHelper
+    public class LearningModeItem
     {
-        private bool _descriptionEnabled;
         private static readonly string _assetsPath =
             Path.Combine(("avares:" + Path.DirectorySeparatorChar + Path.DirectorySeparatorChar + "VocabularyTrainer"), "Assets");
-        
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         public LearningModeItem(string iconFileName, string name, string description, Action clickAction)
         {
@@ -33,21 +27,6 @@ namespace VocabularyTrainer.Models
         internal string Description { get; }
         internal ReactiveCommand<Unit,Unit> ClickCommand { get; }
 
-        internal bool DescriptionEnabled
-        {
-            get => _descriptionEnabled;
-            set
-            {
-                if (_descriptionEnabled == value)
-                    return;
-                _descriptionEnabled = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "") 
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        
         private static Bitmap CreateImage(string fileName)
         {
             string path = Path.Combine(_assetsPath, fileName);
