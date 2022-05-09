7faaa0d390c0f4fd0f2fbc80ae15c31b2886a35c
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-09 23:17:29 +0200

    Implement picking next/previous word and shuffling words
    - Implemented methods for picking the next or previous word and for shuffling the words pool. All that is implemented in `FlashcardsViewModel`.
    - Assigned the methods for picking the next and previous word to the two fitting buttons in FlashcardsView.axaml that are supposed to trigger exactly that.

--- src/ViewModels/LearningModes/FlashcardsViewModel.cs
+++ src/ViewModels/LearningModes/FlashcardsViewModel.cs
@@ -1,9 +1,53 @@
+using System;
+using System.Linq;
+using ReactiveUI;
 using VocabularyTrainer.Models;
 
 namespace VocabularyTrainer.ViewModels.LearningModes
 {
     public sealed class FlashcardsViewModel : LearningModeViewModelBase
     {
-        public FlashcardsViewModel(Lesson lesson) : base(lesson) { }
+        private Word[] _wordsList;
+        private int _wordIndex;
+
+        private Word _currentWord;
+        
+#pragma warning disable CS8618
+        public FlashcardsViewModel(Lesson lesson) : base(lesson)
+#pragma warning restore CS8618
+        {
+            _wordsList = lesson.VocabularyItems.ToArray();
+            NextWord();
+        }
+
+        private Word CurrentWord
+        {
+            get => _currentWord; 
+            set => this.RaiseAndSetIfChanged(ref _currentWord, value);
+        }
+
+        private void PreviousWord()
+        {
+            if (_wordIndex < 0 || _wordIndex >= _wordsList.Length)
+                _wordIndex = _wordsList.Length - 1;
+
+            this.CurrentWord = _wordsList[_wordIndex--];
+            UtilityCollection.Utilities.Log($"{this.CurrentWord.Term}/{this.CurrentWord.Definition}");
+        }
+
+        private void NextWord()
+        {
+            if (_wordIndex >= _wordsList.Length)
+                _wordIndex = 0;
+
+            this.CurrentWord = _wordsList[_wordIndex++];
+            UtilityCollection.Utilities.Log($"{this.CurrentWord.Term}/{this.CurrentWord.Definition}");
+        }
+
+        private void ShuffleWords()
+        {
+            var rnd = new Random();
+            _wordsList = _wordsList.OrderBy(item => rnd.Next()).ToArray();
+        }
     }
 }
