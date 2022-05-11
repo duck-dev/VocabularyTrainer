acdfecc8543e9ce06972590442a2b9dd6790ff6e
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-11 16:40:07 +0200

    Property for `MainWindowViewModel` reference in `ViewModelBase`
    - Added a property to the `ViewModelBase` class, which is a direct reference to the only instance of `MainWindowViewModel`.

--- src/ViewModels/ViewModelBase.cs
+++ src/ViewModels/ViewModelBase.cs
@@ -4,6 +4,11 @@ namespace VocabularyTrainer.ViewModels
 {
     public abstract class ViewModelBase : ReactiveObject
     {
+        protected ViewModelBase()
+        {
+            MainViewModel = MainWindowViewModel.Instance;
+        }
         
+        protected MainWindowViewModel? MainViewModel { get;  }
     }
 }
