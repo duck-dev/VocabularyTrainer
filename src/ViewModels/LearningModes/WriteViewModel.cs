93849a4f18da4379e57c08f003bbb7bffcc2fa54
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-18 23:13:09 +0200

    Add word counter and re-arrange WriteView.axaml
    - Re-arranged the user interface of WriteView.axaml a bit.
    - Added the word counter that's already implemented in FlashcardsView.axaml.
    - `WriteViewModel` inherits `SingleWordViewModelBase`.

--- src/ViewModels/LearningModes/WriteViewModel.cs
+++ src/ViewModels/LearningModes/WriteViewModel.cs
@@ -2,7 +2,7 @@ using VocabularyTrainer.Models;
 
 namespace VocabularyTrainer.ViewModels.LearningModes
 {
-    public sealed class WriteViewModel : LearningModeViewModelBase
+    public sealed class WriteViewModel : SingleWordViewModelBase
     {
         public WriteViewModel(Lesson lesson) : base(lesson) { }
     }
