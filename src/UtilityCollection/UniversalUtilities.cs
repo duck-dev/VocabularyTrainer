50501507539c4a7797ba52d967e18f46e1785f86
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-21 22:09:20 +0200

    Add utility-methods for retrieving resources
    - Added utility-methods for retrieving resources.
        - `GetResource<T>(IResourceNode, string)`: Retrieve a resource from an `IResourceNode` by it's name.
        - `GetResourceFromStyle<TResource, TElement>(TElement, string, int)`: Retrieve a resource from a specific style of the `Styles` of an `IStyleHost`.

--- src/UtilityCollection/UniversalUtilities.cs
+++ src/UtilityCollection/UniversalUtilities.cs
@@ -3,6 +3,9 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
 using VocabularyTrainer.Interfaces;
 using VocabularyTrainer.Models;
 
@@ -99,6 +102,40 @@ namespace VocabularyTrainer.UtilityCollection
             collection.Remove(item);
 
             return true;
+        }
+        
+        /// <summary>
+        /// Retrieves a resource from a specified <see cref="IResourceNode"/> and tries to cast it to the specified type.
+        /// </summary>
+        /// <param name="element">The element to retrieve the resource from.</param>
+        /// <param name="resourceName">The name of the resource you want to retrieve (key).</param>
+        /// <typeparam name="T">The actual type of the resource you want to retrieve.</typeparam>
+        /// <returns>The resource as it's actual type.</returns>
+        public static T? GetResource<T>(IResourceNode element, string resourceName)
+            where T : class
+        {
+            element.TryGetResource(resourceName, out object? resource);
+            return resource as T;
+        }
+
+        /// <summary>
+        /// Retrieves a resource from a specified <see cref="Style"/>, which is in turns contained
+        /// in the <see cref="Styles"/> of an <see cref="IStyleHost"/>, and tries to cast it to the specified type.
+        /// </summary>
+        /// <param name="element">The element to retrieve the resource from.</param>
+        /// <param name="resourceName">The name of the resource you want to retrieve (key).</param>
+        /// <param name="styleIndex">The index of the <see cref="Style"/> inside the <see cref="Styles"/> collection
+        /// of the <paramref name="element"/>.</param>
+        /// <typeparam name="TResource">The actual type of the resource you want to retrieve.</typeparam>
+        /// <typeparam name="TElement">The type of the element to retrieve the resource from.
+        /// This type must implement <see cref="IResourceNode"/>.</typeparam>
+        /// <returns>The resource as it's actual type.</returns>
+        public static TResource? GetResourceFromStyle<TResource, TElement>(TElement? element, string resourceName, int styleIndex)
+            where TResource : class
+            where TElement : IStyleHost, IResourceNode
+        {
+            var styleInclude = element?.Styles[styleIndex] as StyleInclude;
+            return (styleInclude?.Loaded is Style style ? GetResource<TResource>(style, resourceName) : null);
         }
     }
 }
