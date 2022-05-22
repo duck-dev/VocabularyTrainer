b00c1c4f3c890acd9dbe14cc9475903675fe0874
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-22 15:35:29 +0200

    Implement changeable explanation text (solution panel)
    - There is a text in the "SolutionPanel" that quickly describes whether your answer was correct or wrong, to make the panel less "dry". It says either "Correct!" or "Your answer is incorrect. The seeked definition was..." (only temporary texts). Implemented a functionality where the texts are set according to the correctness of the given answer.

--- src/ViewModels/LearningModes/SolutionPanelViewModel.cs
+++ src/ViewModels/LearningModes/SolutionPanelViewModel.cs
@@ -4,8 +4,12 @@ namespace VocabularyTrainer.ViewModels.LearningModes
 {
     public class SolutionPanelViewModel : ViewModelBase
     {
+        private const string ExplanationCorrect = "Correct!";
+        private const string ExplanationWrong = "Your answer is incorrect. The seeked definition was...";
+        
         private string? _term;
         private string? _definition;
+        private string? _explanationText;
 
         internal string? Term
         {
@@ -20,5 +24,14 @@ namespace VocabularyTrainer.ViewModels.LearningModes
         }
         
         internal AnswerViewModelBase? AnswerViewModel { get; init; }
+
+        private string? ExplanationText
+        {
+            get => _explanationText; 
+            set => this.RaiseAndSetIfChanged(ref _explanationText, value);
+        }
+
+        internal void SetExplanationText(bool answerCorrect) 
+            => this.ExplanationText = answerCorrect ? ExplanationCorrect : ExplanationWrong;
     }
 }
