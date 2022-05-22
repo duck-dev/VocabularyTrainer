904ae9f9fd800eb29582c2171921082c1197aae9
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-22 18:00:02 +0200

    Create StarShape.axaml
    - Created a custom shape, which appears in form of a star with 5 corners. The inherited `Shape` class is complemented with various properties and methods in the newly created `StarShape` class that make sure the star looks and behaves correctly.

---
+++ src/Views/Shapes/StarShape.axaml.cs
@@ -0,0 +1,150 @@
+using System;
+using Avalonia;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Metadata;
+
+namespace VocabularyTrainer.Views.Shapes
+{
+    public class StarShape : Shape
+    {
+        private EventHandler? _geometryChangedHandler;
+        
+        public static readonly StyledProperty<double> PathWidthProperty =
+            AvaloniaProperty.Register<StarShape, double>(nameof(PathWidth));
+        
+        public static readonly StyledProperty<double> PathHeightProperty =
+            AvaloniaProperty.Register<StarShape, double>(nameof(PathHeight));
+        
+        public static readonly StyledProperty<double> CanvasLeftProperty =
+            AvaloniaProperty.Register<StarShape, double>(nameof(CanvasLeft));
+        
+        public static readonly StyledProperty<double> CanvasTopProperty =
+            AvaloniaProperty.Register<StarShape, double>(nameof(CanvasTop));
+
+        // public static readonly StyledProperty<Brush> FillProperty =
+        //     AvaloniaProperty.Register<StarShape, Brush>(nameof(Fill));
+        //
+        // public static readonly StyledProperty<Brush> StrokeProperty =
+        //     AvaloniaProperty.Register<StarShape, Brush>(nameof(Stroke));
+        //
+        // public static readonly StyledProperty<double> StrokeThicknessProperty =
+        //     AvaloniaProperty.Register<StarShape, double>(nameof(StrokeThickness));
+        
+        public static readonly StyledProperty<Geometry?> DataProperty =
+            AvaloniaProperty.Register<Path, Geometry?>(nameof(Data));
+        
+        public static readonly StyledProperty<object?> ContentProperty =
+            AvaloniaProperty.Register<StarShape, object?>(nameof(Content));
+        
+        static StarShape()
+        {
+            AffectsGeometry<StarShape>(DataProperty);
+            DataProperty.Changed.AddClassHandler<StarShape>((o, e) => o.DataChanged(e));
+        }
+
+        public StarShape()
+        {
+            InitializeComponent();
+        }
+
+        public double PathWidth
+        {
+            get => GetValue(PathWidthProperty);
+            set => SetValue(PathWidthProperty, value);
+        }
+        
+        public double PathHeight
+        {
+            get => GetValue(PathHeightProperty);
+            set => SetValue(PathHeightProperty, value);
+        }
+
+        public double CanvasLeft
+        {
+            get => GetValue(CanvasLeftProperty);
+            set => SetValue(CanvasLeftProperty, value);
+        }
+        
+        public double CanvasTop
+        {
+            get => GetValue(CanvasTopProperty);
+            set => SetValue(CanvasTopProperty, value);
+        }
+        
+        public Geometry? Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+        
+        private EventHandler? GeometryChangedHandler => _geometryChangedHandler ??= GeometryChanged;
+
+        // public Brush Fill
+        // {
+        //     get => GetValue(FillProperty);
+        //     set => SetValue(FillProperty, value);
+        // }
+        //
+        // public Brush Stroke
+        // {
+        //     get => GetValue(StrokeProperty);
+        //     set => SetValue(StrokeProperty, value);
+        // }
+        //
+        // public double StrokeThickness
+        // {
+        //     get => GetValue(StrokeThicknessProperty);
+        //     set => SetValue(StrokeThicknessProperty, value);
+        // }
+        
+        /// <summary>
+        /// Gets or sets the content to display.
+        /// </summary>
+        [Content]
+        public object? Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+        
+        protected override Geometry? CreateDefiningGeometry() => Data;
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+
+            if (Data is not null)
+                Data.Changed += GeometryChangedHandler;
+        }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+            if (Data is not null)
+                Data.Changed -= GeometryChangedHandler;
+        }
+
+        private void DataChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            if (VisualRoot is null)
+                return;
+
+            var oldGeometry = (Geometry?)e.OldValue;
+            var newGeometry = (Geometry?)e.NewValue;
+
+            if (oldGeometry is not null)
+                oldGeometry.Changed -= GeometryChangedHandler;
+            if (newGeometry is not null)
+                newGeometry.Changed += GeometryChangedHandler;
+        }
+
+        private void GeometryChanged(object? sender, EventArgs e) => InvalidateGeometry();
+    }
+}
