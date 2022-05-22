8e46c30b0993b3a3490a4516163e8f36493b4a15
Duck <61479436+duck-dev@users.noreply.github.com>
2022-05-22 15:33:45 +0200

    Improve solution panel functionality
    - Moved the functionality for opening the solution panel from `ShowSolution` to a separate method, as the solution panel won't only be shown when the user asks for the solution. The method...
        - ... creates a new `SolutionPanelViewModel` if there isn't one already.
        - ... sets the panel's `Term` and `Definition`.
        - ... sets the explanation-text by calling the `SetExplanationText` method (the implementation will follow in the next commit).
        - ... sets the `AnswerColor`, depending on whether the answer was correct or not (green, red).
        - ... sets `IsSolutionShown` to `true`.
    - Created 3 readonly properties that store the selected black, green -and red colors to avoid duplicate code. It tries to retrieve the color from the resources, which should usually work, but if it fails, it assigns a fallback color.

--- src/ViewModels/LearningModes/BaseClasses/AnswerViewModelBase.cs
+++ src/ViewModels/LearningModes/BaseClasses/AnswerViewModelBase.cs
@@ -14,15 +14,21 @@ namespace VocabularyTrainer.ViewModels.LearningModes
         private SolutionPanelViewModel? _solutionPanel;
         private SolidColorBrush _answerColor;
         
-        private readonly SolidColorBrush _fallbackColorBlack = new(Color.Parse("#000000"));
-        private readonly SolidColorBrush _fallbackColorRed = new(Color.Parse("#FF0000"));
+        private readonly SolidColorBrush _blackColor 
+            = Utilities.GetResourceFromStyle<SolidColorBrush,Application>(Application.Current, "OppositeAccent", 1) 
+              ?? new(Color.Parse("#000000"));
+        private readonly SolidColorBrush _greenColor
+            = Utilities.GetResourceFromStyle<SolidColorBrush, Application>(Application.Current, "MainGreen", 1)
+              ?? new(Color.Parse("#0CA079"));
+        private readonly SolidColorBrush _redColor 
+            = Utilities.GetResourceFromStyle<SolidColorBrush, Application>(Application.Current, "MainRed", 1) 
+              ?? new(Color.Parse("#FF0000"));
 
         public event EventHandler? ReadyToFocus;
 
         protected AnswerViewModelBase(Lesson lesson) : base(lesson)
         {
-            _answerColor = this.AnswerColor = Utilities.GetResourceFromStyle<SolidColorBrush,Application>
-                (Application.Current, "OppositeAccent", 1) ?? _fallbackColorBlack;
+            _answerColor = this.AnswerColor = _blackColor;
             this.IsSolutionShown = false;
         }
         
@@ -56,27 +62,32 @@ namespace VocabularyTrainer.ViewModels.LearningModes
         
         protected void CheckAnswer()
         {
-            
+            OpenSolutionPanel(this.DisplayedTerm, this.Answer, true); // TODO: Check answer
         }
         
         protected void ShowSolution()
         {
-            this.SolutionPanel ??= new SolutionPanelViewModel
-            {
-                AnswerViewModel = this
-            };
-            this.SolutionPanel.Term = this.DisplayedTerm;
-            this.SolutionPanel.Definition = this.Definition;
-            
-            this.IsSolutionShown = true;
-            this.AnswerColor = Utilities.GetResourceFromStyle<SolidColorBrush, Application>
-                (Application.Current, "MainRed", 1) ?? _fallbackColorRed;
+            OpenSolutionPanel(this.DisplayedTerm, this.Definition, false);
         }
 
         protected override void PickWord(bool resetKnownWords = false)
         {
             base.PickWord(resetKnownWords);
             ReadyToFocus?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OpenSolutionPanel(string? term, string? definition, bool answerCorrect)
+        {
+            this.SolutionPanel ??= new SolutionPanelViewModel
+            {
+                AnswerViewModel = this
+            };
+            this.SolutionPanel.Term = term;
+            this.SolutionPanel.Definition = definition;
+            this.SolutionPanel.SetExplanationText(answerCorrect);
+
+            this.AnswerColor = answerCorrect ? _greenColor : _redColor;
+            this.IsSolutionShown = true;
         }
     }
 }
