4f86d0a918a0673fcda86084687a97df5706b040
Duck <61479436+duck-dev@users.noreply.github.com>
2022-06-22 15:37:31 +0200

    Implement logic for dialogs
    - Created a DialogViewModelBase.cs file containing a `DialogViewModelBase` class that inherits `ViewModelBase`. This class contains properties and methods that are generally available for all specific implementations of a dialog ViewModel, including a `Title`, `ButtonColors`, `ButtonColorsHover`, `ButtonTextColors`, `ButtonTexts`, `IgnoreDialog` and `CloseDialog()`.
    - Created a ConfirmationDialogViewModel.cs file containing a `ConfirmationDialogViewModel` class that inherits `DialogViewModelBase`.

---
+++ src/ViewModels/BaseClasses/DialogViewModelBase.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Media;
+using VocabularyTrainer.Extensions;
+
+namespace VocabularyTrainer.ViewModels.BaseClasses;
+
+public abstract class DialogViewModelBase : ViewModelBase
+{
+    protected DialogViewModelBase(string title,
+        IEnumerable<SolidColorBrush> buttonColors,
+        IEnumerable<SolidColorBrush> buttonTextColors,
+        IEnumerable<string> buttonTexts)
+    {
+        this.Title = title;
+        this.ButtonColors = buttonColors.ToArray();
+        this.ButtonTextColors = buttonTextColors.ToArray();
+        this.ButtonTexts = buttonTexts.ToArray();
+    }
+        
+    protected string Title { get; init; }
+        
+    protected SolidColorBrush[] ButtonColors { get; }
+    protected SolidColorBrush[] ButtonColorsHover
+        => ButtonColors.Select(x => new SolidColorBrush(x.Color.DarkenColor(0.1f))).ToArray();
+    protected SolidColorBrush[] ButtonTextColors { get; }
+        
+    protected string[] ButtonTexts { get; }
+        
+    protected bool IgnoreDialog { get; set; }
+        
+    protected static void CloseDialog()
+    {
+        if (MainWindowViewModel.Instance is not { } mainInstance)
+            return;
+        mainInstance.CurrentDialog = null;
+    }
+}
---
+++ src/ViewModels/Dialogs/ConfirmationDialogViewModel.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Media;
+using VocabularyTrainer.ViewModels.BaseClasses;
+
+namespace VocabularyTrainer.ViewModels.Dialogs;
+
+public class ConfirmationDialogViewModel : DialogViewModelBase
+{
+    private enum ActionType
+    {
+        Confirm,
+        Cancel
+    }
+        
+    private readonly Action? _confirmAction;
+    private readonly Action? _cancelAction;
+
+    public ConfirmationDialogViewModel(string title,
+        IEnumerable<SolidColorBrush> buttonColors,
+        IEnumerable<SolidColorBrush> buttonTextColors,
+        IEnumerable<string> buttonTexts,
+        Action? confirmAction,
+        Action? cancelAction = null) : base(title, buttonColors, buttonTextColors, buttonTexts)
+    {
+        _confirmAction = confirmAction;
+        _cancelAction = cancelAction;
+    }
+
+    public ConfirmationDialogViewModel(string title,
+        IEnumerable<Color> buttonColors,
+        IEnumerable<Color> buttonTextColors,
+        IEnumerable<string> buttonTexts,
+        Action? confirmAction,
+        Action? cancelAction = null) : this(title,
+                                            buttonColors.Select(x => new SolidColorBrush(x)),
+                                            buttonTextColors.Select(x => new SolidColorBrush(x)),
+                                            buttonTexts,
+                                            confirmAction,
+                                            cancelAction)
+    { }
+
+    private void Command(ActionType actionType)
+    {
+        CloseDialog();
+        switch (actionType)
+        {
+            case ActionType.Confirm:
+                //IgnoreDialog(); // Set a variable in the settings that makes sure this dialog will be ignored in the future
+                _confirmAction?.Invoke();
+                break;
+            case ActionType.Cancel:
+                _cancelAction?.Invoke();
+                break;
+        }
+    }
+}
