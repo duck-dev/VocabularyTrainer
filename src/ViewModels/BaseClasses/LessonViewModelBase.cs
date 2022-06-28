duck-dev/VocabularyTrainer

commit 97ff7763aa849346215b93fe40be49f43b2db33c

Fix issue: "Custom" lesson options being displayed as "High"
- Fixed an issue: If custom lesson options had been selected (selected tolerance: "Custom") and the lesson had been opened, the displayed settings would be as if the tolerance was set to "High" (CorrectionSteps = 3, all the other properties = true).

src/ViewModels/BaseClasses/LessonViewModelBase.cs
@@ -16,6 +16,7 @@ public abstract class LessonViewModelBase : ViewModelBase
     private LessonOptions _currentOptions = null!;
     private bool _individualSettingsEnabled;
     private float _individualSettingsOpacity;
+    private bool _initializedTolerance;
     
     protected Tuple<string, ErrorTolerance>[] ErrorToleranceTemplates
     {
@@ -33,6 +34,12 @@ public abstract class LessonViewModelBase : ViewModelBase
             this.RaiseAndSetIfChanged(ref _selectedTolerance, value);
             
             ErrorTolerance newTolerance = ErrorToleranceTemplates[_selectedTolerance].Item2;
+            if (!_initializedTolerance && MainWindowViewModel.CurrentLesson is { } lesson && newTolerance != lesson.Options.CurrentTolerance)
+            {
+                _initializedTolerance = true;
+                SelectedTolerance = (int)lesson.Options.CurrentTolerance;
+                return;
+            }
             ChangeTolerance(newTolerance);
             IndividualSettingsEnabled = newTolerance == ErrorTolerance.Custom;
         }

src/ViewModels/LessonListViewModel.cs
@@ -37,9 +37,11 @@ public class LessonListViewModel : ViewModelBase
         if (MainViewModel is null)
             return;
 
-        var lessonViewModel = new LessonViewModel(lesson);
+        var lessonViewModel = new LessonViewModel(lesson)
+        {
+            SelectedTolerance = (int)lesson.Options.CurrentTolerance
+        };
         MainViewModel.Content = lessonViewModel;
-        lessonViewModel.SelectedTolerance = (int)lesson.Options.CurrentTolerance;
         MainWindowViewModel.CurrentLesson = lesson;
     }
 

commit 5ab65f6de8c2e9c1b42addf62587a6664c8f5f71

Fix NullReferenceException in LessonViewModelBase.cs
- Fixed a NullReferenceException that's occurring with the new fix for the issue that was introduced in commit 97ff776 (Fix issue: "Custom" lesson options being displayed as "High") yesterday. The exception only occurred if `AddLessonViewModel` was tried to be opened. Now it won't occur anymore!

src/ViewModels/BaseClasses/LessonViewModelBase.cs
@@ -34,7 +34,8 @@ public abstract class LessonViewModelBase : ViewModelBase
             this.RaiseAndSetIfChanged(ref _selectedTolerance, value);
             
             ErrorTolerance newTolerance = ErrorToleranceTemplates[_selectedTolerance].Item2;
-            if (!_initializedTolerance && MainWindowViewModel.CurrentLesson is { } lesson && newTolerance != lesson.Options.CurrentTolerance)
+            if (this is not AddLessonViewModel && !_initializedTolerance && MainWindowViewModel.CurrentLesson is { } lesson 
+                && newTolerance != lesson.Options.CurrentTolerance)
             {
                 _initializedTolerance = true;
                 SelectedTolerance = (int)lesson.Options.CurrentTolerance;