54fc854632b993e34a2d59815d7ab835358e91d6
Duck <61479436+duck-dev@users.noreply.github.com>
2022-07-12 21:15:01 +0200

    Add more generic version of `Clone` extension
    - Added a more generic version of the `Clone` extension method in CollectionExtensions.cs. Instead of extending an `IEnumerable<T>` and returning the same type, this new extension method extends `TCollection` (`where TCollection : class, IEnumerable<TItem>` and `where TItem : ICloneable`). The return type is nullable.
     The original extension method stays.

--- src/Extensions/CollectionExtensions.cs
+++ src/Extensions/CollectionExtensions.cs
@@ -15,4 +15,19 @@ public static partial class Extensions
     /// <returns>The cloned collection.</returns>
     public static IEnumerable<T> Clone<T>(this IEnumerable<T> collection) where T : ICloneable 
         => collection.Select(x => (T)x.Clone());
+    
+    /// <summary>
+    /// Create a deep copy of an <see cref="IEnumerable{T}"/> collection.
+    /// </summary>
+    /// <param name="collection">The list to be cloned.</param>
+    /// <typeparam name="TCollection">The type of the extended collection, which must implement <see cref="IEnumerable{T}"/>.</typeparam>
+    /// <typeparam name="TItem">The type of the elements inside the collection. All of them ought to inherit <see cref="ICloneable"/></typeparam>
+    /// <returns>The cloned collection.</returns>
+    public static TCollection? Clone<TCollection, TItem>(this TCollection collection)
+        where TCollection : class, IEnumerable<TItem> 
+        where TItem : ICloneable
+    {
+        var newEnumerable = collection.Select(x => (TItem)x.Clone());
+        return Activator.CreateInstance(typeof(TCollection), newEnumerable) as TCollection;
+    }
 }
