0209bc34bf2caa365b6dc0e670b7bc21347785d6
Duck <61479436+duck-dev@users.noreply.github.com>
2022-08-31 18:20:55 +0200

    [NOT TESTED] Fix learning mode "Multiple Choice"
    - Many issues have been fixed in the learning mode "Multiple Choice". First tests have shown that the mechanic works as expected in several cases, but thorough tests are yet to be done.

--- src/ViewModels/LearningModes/MultipleChoiceViewModel.cs
+++ src/ViewModels/LearningModes/MultipleChoiceViewModel.cs
@@ -1,10 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using DynamicData;
+using ReactiveUI;
 using VocabularyTrainer.Enums;
+using VocabularyTrainer.Extensions;
 using VocabularyTrainer.Models;
 using VocabularyTrainer.UtilityCollection;
 
@@ -22,12 +23,18 @@ public sealed class MultipleChoiceViewModel : AnswerViewModelBase
 
     private static readonly int[] _similarWordLimits = { 1, 4 };
 
+    private ObservableCollection<string> _choices = new();
+
     public MultipleChoiceViewModel(Lesson lesson) : base(lesson)
     {
         VerifyAndSetItem(SetWord);
     }
 
-    private ObservableCollection<string> Choices { get; } = new();
+    private ObservableCollection<string> Choices
+    {
+        get => _choices; 
+        set => this.RaiseAndSetIfChanged(ref _choices, value);
+    }
 
     protected override void Initialize(bool initializeWords)
     {
@@ -50,7 +57,8 @@ public sealed class MultipleChoiceViewModel : AnswerViewModelBase
     protected override void SetWord()
     {
         base.SetWord();
-        AddPossibleDefinitions();
+        var definitions = DeterminePossibleDefinitions();
+        Choices = new ObservableCollection<string>(definitions);
     }
 
     protected override void ShuffleWords()
@@ -66,16 +74,17 @@ public sealed class MultipleChoiceViewModel : AnswerViewModelBase
         Utilities.ChangeLearningState(CurrentWord, this, answer.Equals(Definition), considerOverallState: true);
     }
 
-    private void AddPossibleDefinitions()
+    private IEnumerable<string> DeterminePossibleDefinitions()
     {
-        PossibleDefinitions = new List<string> {Definition};
+        Choices.Clear();
+        Choices.Add(Definition);
         var allWords = new List<Word>(WordsList);
         allWords.Remove(CurrentWord); // No duplicate definition in choices (prevent choosing seeked definition again)
         
         int similarWordProbability = Random.Shared.Next(0, _similarWordLimits[^1]);
         if (similarWordProbability < _similarWordLimits[0])
         {
-            var similarWords = allWords.Where(x => Utilities.LevenshteinDistance(x.Definition, Definition) <= 2).ToList();
+            List<Word> similarWords = allWords.Where(x => Utilities.LevenshteinDistance(x.Definition, Definition) <= 2).ToList();
             if (similarWords.Count > 0)
             {
                 int randomSimilarWord = Random.Shared.Next(0, similarWords.Count);
@@ -86,27 +95,32 @@ public sealed class MultipleChoiceViewModel : AnswerViewModelBase
         }
 
         if (Choices.Count == 4)
-            return; // TODO: Do something with `list`
+            return Choices.Shuffle();
 
         PartOfSpeech partOfSpeech = CurrentWord.SelectedPartOfSpeech;
-        IEnumerable<Word> possibleWords = allWords.Where(x => x.SelectedPartOfSpeech == partOfSpeech);
-        List<string> possibleDefinitions = possibleWords.Select(x => x.Definition).ToList();
         int threshold = 4 - Choices.Count;
+
+        if (partOfSpeech == PartOfSpeech.None && CompleteMissingDefinitions(threshold, allWords, Choices, allWords))
+            return Choices.Shuffle(); 
+        
+        IList<Word> possibleWords = allWords.Where(x => x.SelectedPartOfSpeech == partOfSpeech).ToList();
+        List<string> possibleDefinitions = possibleWords.Select(x => x.Definition).ToList();
         if (possibleDefinitions.Count == threshold)
         {
             Choices.AddRange(possibleDefinitions);
+            return Choices.Shuffle();
         }
         else if (possibleDefinitions.Count > threshold)
         {
-            if (CompleteMissingDefinitions(threshold, possibleDefinitions, Choices, allWords))
-                return;
+            if (CompleteMissingDefinitions(threshold, possibleWords, Choices, allWords))
+                return Choices.Shuffle();
         }
         else
         {
-            if (CompleteMissingDefinitions(threshold, possibleDefinitions, Choices, allWords))
-                return;
+            if (CompleteMissingDefinitions(threshold, possibleWords, Choices, allWords))
+                return Choices.Shuffle();
+
             threshold = 4 - Choices.Count;
-            var completedDefinitions = new List<string>(possibleDefinitions);
             int alternativeIndex = 0;
             while (completedDefinitions.Count < threshold)
             {
@@ -114,37 +128,48 @@ public sealed class MultipleChoiceViewModel : AnswerViewModelBase
                     alternativeIndex < _alternativePartOfSpeeches[partOfSpeech].Length)
                 {
                     PartOfSpeech alternative = _alternativePartOfSpeeches[partOfSpeech][alternativeIndex];
-                    possibleWords = allWords.Where(x => x.SelectedPartOfSpeech == alternative);
-                    if (!possibleWords.Any())
+                    possibleWords = allWords.Where(x => x.SelectedPartOfSpeech == alternative).ToList();
+                    possibleDefinitions = possibleWords.Select(x => x.Definition).ToList();
+                    if (!possibleDefinitions.Any())
                     {
                         alternativeIndex++;
                         continue;
                     }
-                    possibleDefinitions = possibleWords.Select(x => x.Definition).ToList();
-                    if (CompleteMissingDefinitions(threshold, possibleDefinitions, Choices, allWords))
-                        return;
+                    if (CompleteMissingDefinitions(threshold, possibleWords, Choices, allWords))
+                        return Choices.Shuffle();
 
+                    threshold = 4 - Choices.Count;
                     alternativeIndex++;
                     continue;
                 }
-
-                if (CompleteMissingDefinitions(threshold, allWords.Select(x => x.Definition).ToList(), Choices, allWords))
-                    return;
+                
+                if (CompleteMissingDefinitions(threshold, allWords, Choices, allWords))
+                    return Choices.Shuffle();
                 throw new Exception("Something went terribly wrong. The choices couldn't be filled. [MULTIPLE CHOICE]");
             }
         }
+        
+        throw new Exception("Something went terribly wrong. The choices couldn't be filled. [MULTIPLE CHOICE]");
     }
 
-    private static bool CompleteMissingDefinitions(int iterationCount, IList<string> collection, ICollection<string> targetList, IList allWords)
+    private static bool CompleteMissingDefinitions(int iterationCount, IList<Word> wordCollection, ICollection<string> targetList, ICollection<Word> allWords)
     {
+        IList<string> collection = wordCollection.Select(x => x.Definition).ToList();
         for (int i = 0; i < iterationCount; i++)
         {
-            if (collection.Count <= 0)
+            if (wordCollection.Count <= 0)
                 return false;
-            int randomIndex = Random.Shared.Next(0, collection.Count);
+
+            int randomIndex = Random.Shared.Next(0, wordCollection.Count);
             targetList.Add(collection[randomIndex]);
+            if (targetList.Count == 4)
+                return true;
+            
             collection.RemoveAt(randomIndex);
-            allWords.RemoveAt(randomIndex);
+            allWords.Remove(wordCollection[randomIndex]);
+            if(!ReferenceEquals(allWords, wordCollection))
+                wordCollection.RemoveAt(randomIndex); // Must be after the line above, so that the word doesn't disappear
+                                                      // from this collection before being accessed to remove it from `allWords`
         }
 
         return targetList.Count == 4;
