07a91d96fae16d96271705b9b8a93ae64179e91a
Duck <61479436+duck-dev@users.noreply.github.com>
2022-11-16 21:59:22 +0100

    Add star to mark word as difficult to SolutionPanel
    - Added a star, as it's already implemented in all learning modes, to the so-called "Solution Panel" (SolutionPanelView.axaml), which is the view where the solution gets displayed in learning modes that request a written or selected answer to a displayed term and where it also displays whether the given answer was correct or not. There, one can also mark the answered word as difficult. The only difference is that the star's stroke (border) is white instead of black, because the background is already dark.

--- src/Views/LearningModes/SecondaryElements/SolutionPanelView.axaml.cs
+++ src/Views/LearningModes/SecondaryElements/SolutionPanelView.axaml.cs
@@ -1,9 +1,11 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using VocabularyTrainer.ViewModels.LearningModes;
 
 namespace VocabularyTrainer.Views.LearningModes;
 
-public partial class SolutionPanelView : UserControl
+public class SolutionPanelView : UserControl
 {
     public SolutionPanelView()
     {
@@ -13,5 +15,17 @@ public partial class SolutionPanelView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void OnStarChecked(object? sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is SolutionPanelViewModel dataContext)
+            dataContext.AnswerViewModel?.SetDifficultTerm(true);
+    }
+
+    private void OnStarUnchecked(object? sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is SolutionPanelViewModel dataContext)
+            dataContext.AnswerViewModel?.SetDifficultTerm(false);
     }
 }
