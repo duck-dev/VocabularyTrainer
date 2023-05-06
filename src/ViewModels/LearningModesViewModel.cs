abbd5ea223d488d1e443d6e6a34a935f32c77458
Duck <61479436+duck-dev@users.noreply.github.com>
2023-05-06 12:15:32 +0200

    Add display for learning progress in LessonModesView.axaml
    - Added two progress bars for the learning progress (known and wrong words) to the LessonModesView, which is displayed in the LessonView (LessonView.axaml), meaning the view of an opened already existing lesson, which can be edited.

--- src/ViewModels/LearningModesViewModel.cs
+++ src/ViewModels/LearningModesViewModel.cs
@@ -1,12 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using ReactiveUI;
 using VocabularyTrainer.Enums;
+using VocabularyTrainer.Extensions;
 using VocabularyTrainer.Models;
+using VocabularyTrainer.UtilityCollection;
 using VocabularyTrainer.ViewModels.LearningModes;
 
 namespace VocabularyTrainer.ViewModels;
 
 public sealed class LearningModesViewModel : ViewModelBase
 {
+    private readonly Lesson? _currentLesson;
+    private int _knownWords;
+    private int _wrongWords;
+    
+    public LearningModesViewModel()
+    {
+        _currentLesson = MainWindowViewModel.CurrentLesson;
+        if (_currentLesson is null)
+            return;
+
+        var words = _currentLesson.VocabularyItems;
+        words.CollectionChanged += (sender, args) => RetrieveLearningProgress(words);
+        RetrieveLearningProgress(words);
+    }
+    
     private LearningModeItem[] LearningModes { get; } =
     {
         new ("Flashcards.png", "Flashcards", "Memorize vocabulary super fast by flipping flashcards.",
@@ -22,6 +42,27 @@ public sealed class LearningModesViewModel : ViewModelBase
             "Do you prefer just looking at a list of words with their term and definition? Then this is your choice.",
             OpenLearningMode<VocabularyListViewModel>, LearningModeType.VocabularyList),
     };
+
+    private int MaximumItems => _currentLesson is not { } lesson ? 0 : lesson.VocabularyItems.Count;
+
+    private int KnownWords
+    {
+        get => _knownWords; 
+        set => this.RaiseAndSetIfChanged(ref _knownWords, value);
+    }
+
+    private int WrongWords
+    {
+        get => _wrongWords; 
+        set => this.RaiseAndSetIfChanged(ref _wrongWords, value);
+    }
+
+    private void RetrieveLearningProgress(ICollection<Word> words)
+    {
+        this.KnownWords = words.Count(x => x.LearningStatus.CustomHasFlag(Utilities.KnownFlags));
+        this.WrongWords = words.Count(x => x.LearningStatus.CustomHasFlag(Utilities.WrongFlags));
+        this.RaisePropertyChanged(nameof(MaximumItems));
+    }
 
     private static void OpenLearningMode<T>() where T : LearningModeViewModelBase
     {
